07fa252c7490bcb68c8b555f33c5094c214dcf2f
MadmanMigz <202310011@fit.edu.ph>
2025-06-19 16:24:15 +0800

    feat (sql): added table for users.

---
+++ database/users.model.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    full_name VARCHAR(100),
+    role VARCHAR(50),
+    group_name VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
