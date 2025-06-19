7792c5be22dbd3d58b1c522999cf2322ef0dd252
MadmanMigz <202310011@fit.edu.ph>
2025-06-19 16:24:41 +0800

    feat (sql): added table for projects.

---
+++ database/projects.model.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS projects (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    created_by INTEGER REFERENCES users (id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
