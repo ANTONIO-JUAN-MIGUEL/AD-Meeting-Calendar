71650fe7b76bd91d0d9f829bf72709c9ba6a0f01
MadmanMigz <202310011@fit.edu.ph>
2025-06-23 10:54:43 +0800

    refactor (sql): Changed "projects" to "meetings"

--- database/meetings.model.sql
+++ database/meetings.model.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS meetings (
     id SERIAL PRIMARY KEY,
-    project_id INTEGER REFERENCES projects (id),
     title VARCHAR(100) NOT NULL,
     agenda TEXT,
     scheduled_at TIMESTAMP NOT NULL,
