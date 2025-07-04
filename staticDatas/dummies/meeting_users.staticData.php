9a9f319e5f13f2a707e65825600ac54a9d6548f8
MadmanMigz <202310011@fit.edu.ph>
2025-07-04 12:14:52 +0800

    feat (staticData): Added requirements from step 10.

---
+++ staticDatas/dummies/meeting_users.staticData.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    [
+        'meeting_id' => 1,
+        'user_id' => 1,
+        'role' => 'facilitator'
+    ],
+    [
+        'meeting_id' => 1,
+        'user_id' => 2,
+        'role' => 'participant'
+    ],
+    [
+        'meeting_id' => 2,
+        'user_id' => 3,
+        'role' => 'observer'
+    ]
+];
