f51899b617f8af5c690a706098614e2ddc58aa15
MadmanMigz <202310011@fit.edu.ph>
2025-07-04 14:29:51 +0800

    refactor (landing page): updated code for new UI.

--- index.php
+++ index.php
@@ -1,31 +1,35 @@
+<?php
+require_once 'bootstrap.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <title>Service Status</title>
-    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="/assets/css/global.css">
 </head>
 
 <body>
-    <h1>Service Status</h1>
 
-    <h2>MongoDB:</h2>
-    <ul>
-        <li><?php include HANDLERS_PATH . 'mongodbChecker.handler.php'; ?></li>
-    </ul>
+    <?php include_once BASE_PATH . '/components/partials/navbar.php'; ?>
+    <div class="main-container">
 
-    <h2>PostgreSQL:</h2>
-    <ul>
-        <li><?php include HANDLERS_PATH . 'postgreChecker.handler.php'; ?></li>
-    </ul>
+        <h1>Service Status</h1>
 
-    <h2>Automation:</h2>
-    <ul>
-        <li>✅ PostgreSQL Reset script is working via: <code>composer postgresql:reset</code></li>
-        <li>✅ PostgreSQL Seeder script is working via: <code>composer postgresql:seed</code></li>
-        <li>✅ PostgreSQL Migration script is working via: <code>composer postgresql:migrate</code></li>
-    </ul>
+        <h2>MongoDB:</h2>
+        <?php include HANDLERS_PATH . 'mongodbChecker.handler.php'; ?>
+
+        <h2>PostgreSQL:</h2>
+        <?php include HANDLERS_PATH . 'postgreChecker.handler.php'; ?>
+
+        <h2>Automation:</h2>
+        <ul>
+            <li>✅ PostgreSQL Reset script is working via <code>composer postgresql:reset</code></li>
+            <li>✅ PostgreSQL Seeder script is working via <code>composer postgresql:seed</code></li>
+            <li>✅ PostgreSQL Migration script is working via <code>composer postgresql:migrate</code></li>
+        </ul>
+    </div>
 </body>
 
 </html>
