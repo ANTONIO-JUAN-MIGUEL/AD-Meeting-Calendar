51ac8703f6130c2ca62bfb431af47e0dbc27a147
MadmanMigz <202310011@fit.edu.ph>
2025-07-04 12:15:04 +0800

    feat (staticData): Added requirements from step 10.

---
+++ staticDatas/dummies/users.staticData.php
@@ -0,0 +1,24 @@
+<?php
+return [
+    [
+        'username' => 'john.smith',
+        'first_name' => 'John',
+        'last_name' => 'Smith',
+        'password' => 'p@ssW0rd1234',
+        'role' => 'designer'
+    ],
+    [
+        'username' => 'jane.doe',
+        'first_name' => 'Jane',
+        'last_name' => 'Doe',
+        'password' => 'securePass!@#',
+        'role' => 'developer'
+    ],
+    [
+        'username' => 'admin.user',
+        'first_name' => 'Admin',
+        'last_name' => 'User',
+        'password' => 'adminPass123',
+        'role' => 'admin'
+    ]
+];
