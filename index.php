ffce5b36bcab489ba1a926b7389d107be61044f2
MadmanMigz <202310011@fit.edu.ph>
2025-07-04 12:31:45 +0800

    refactor (index): updated code for styling.

--- index.php
+++ index.php
@@ -1,18 +1,31 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <title>Checker</title>
+    <meta charset="UTF-8">
+    <title>Service Status</title>
+    <link rel="stylesheet" href="assets/css/index.css">
 </head>
 
 <body>
     <h1>Service Status</h1>
 
     <h2>MongoDB:</h2>
-    <?php include HANDLERS_PATH . 'mongodbChecker.handler.php'; ?>
+    <ul>
+        <li><?php include HANDLERS_PATH . 'mongodbChecker.handler.php'; ?></li>
+    </ul>
 
     <h2>PostgreSQL:</h2>
-    <?php include HANDLERS_PATH . 'postgreChecker.handler.php'; ?>
+    <ul>
+        <li><?php include HANDLERS_PATH . 'postgreChecker.handler.php'; ?></li>
+    </ul>
+
+    <h2>Automation:</h2>
+    <ul>
+        <li>✅ PostgreSQL Reset script is working via: <code>composer postgresql:reset</code></li>
+        <li>✅ PostgreSQL Seeder script is working via: <code>composer postgresql:seed</code></li>
+        <li>✅ PostgreSQL Migration script is working via: <code>composer postgresql:migrate</code></li>
+    </ul>
 </body>
 
 </html>
