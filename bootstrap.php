c692802ac571313670ce35b6ce4b543828cd4bc5
MadmanMigz <202310011@fit.edu.ph>
2025-07-04 14:30:52 +0800

    refactor (bootstrap): updated bootstrap code for pathway.

--- bootstrap.php
+++ bootstrap.php
@@ -1,13 +1,9 @@
 <?php
 define('BASE_PATH', realpath(__DIR__));
-
 if (!defined('UTILS_PATH')) {
     define('UTILS_PATH', BASE_PATH . '/utils/');
 }
-
 define('VENDOR_PATH', BASE_PATH . '/vendor/');
 define('HANDLERS_PATH', BASE_PATH . '/handlers/');
 define('DUMMIES_PATH', BASE_PATH . '/staticDatas/dummies/');
-
 chdir(BASE_PATH);
-
