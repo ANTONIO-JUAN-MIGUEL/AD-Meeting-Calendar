ffedafcedf287d441abaf1a06a67fa2549b95f65
MadmanMigz <202310011@fit.edu.ph>
2025-07-04 15:55:35 +0800

    refactor (sql): updated to correct table.

--- database/users.model.sql
+++ database/users.model.sql
@@ -1,10 +1,9 @@
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(100) UNIQUE,
-    full_name VARCHAR(100),
-    role VARCHAR(50),
-    group_name VARCHAR(50),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS public."users" (
+    id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid (),
+    first_name varchar(225) NOT NULL,
+    middle_name varchar(225),
+    last_name varchar(225) NOT NULL,
+    password varchar(225) NOT NULL,
+    username varchar(225) NOT NULL UNIQUE,
+    role varchar(225) NOT NULL
 );
