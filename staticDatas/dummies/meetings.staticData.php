74c0fdc0a49e8803e25f4708cf086529981cccba
MadmanMigz <202310011@fit.edu.ph>
2025-07-04 12:14:58 +0800

    feat (staticData): Added requirements from step 10.

---
+++ staticDatas/dummies/meetings.staticData.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    [
+        'title' => 'Weekly Sync',
+        'agenda' => 'Project progress, blockers, goals',
+        'scheduled_at' => '2025-07-01 10:00:00'
+    ],
+    [
+        'title' => 'Design Review',
+        'agenda' => 'Feedback on the new UI mockups',
+        'scheduled_at' => '2025-07-03 15:00:00'
+    ]
+];
