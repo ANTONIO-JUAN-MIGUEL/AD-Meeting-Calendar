8c2c9e0e2ca90ba01c71fe9951db4b7e2f6933bb
MadmanMigz <202310011@fit.edu.ph>
2025-07-08 17:11:22 +0800

    updated code for new format and using required folders/files.

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,18 @@
 <?php
 define('BASE_PATH', realpath(__DIR__));
-if (!defined('UTILS_PATH')) {
-    define('UTILS_PATH', BASE_PATH . '/utils/');
-}
+define('UTILS_PATH', BASE_PATH . '/utils/');
 define('VENDOR_PATH', BASE_PATH . '/vendor/');
 define('HANDLERS_PATH', BASE_PATH . '/handlers/');
 define('DUMMIES_PATH', BASE_PATH . '/staticDatas/dummies/');
+define('COMPONENTS_PATH', BASE_PATH . '/components/');
+define('LAYOUTS_PATH', BASE_PATH . '/layouts/');
+define('TEMPLATES_PATH', COMPONENTS_PATH . 'templates/');
+define('SHARED_PATH', COMPONENTS_PATH . 'shared/');
+define('ERRORS_PATH', BASE_PATH . '/errors/');
+
 chdir(BASE_PATH);
+
+// ✅ Load .env
+require_once VENDOR_PATH . '/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
+$dotenv->load();
