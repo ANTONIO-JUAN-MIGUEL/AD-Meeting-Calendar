d145683192cf16c681e0267828db37d7fbb5f918
MadmanMigz <202310011@fit.edu.ph>
2025-07-08 17:09:05 +0800

    refactor (pages): updated pages to follow requirements.

--- pages/status/index.php
+++ pages/status/index.php
@@ -1,40 +1,30 @@
 <?php
 require_once '../../bootstrap.php';
+
+ob_start();
 ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <title>Service Status</title>
-    <link rel="stylesheet" href="/assets/css/global.css">
-</head>
+<h1>Service Status</h1>
 
-<body>
+<h2>MongoDB:</h2>
+<?php include HANDLERS_PATH . 'mongodbChecker.handler.php'; ?>
 
-    <?php include_once BASE_PATH . '/components/partials/navbar.php'; ?>
+<h2>PostgreSQL:</h2>
+<?php include HANDLERS_PATH . 'postgreChecker.handler.php'; ?>
 
-    <div class="main-container">
-        <h1>Service Status</h1>
+<h2>Automation:</h2>
+<ul>
+    <li>✅ PostgreSQL Reset USING DOCKER is working via:
+        <code>docker exec -it web-meeting-calendar-service php utils/dbResetPostgresql.util.php</code>
+    </li>
+    <li>✅ PostgreSQL Seeder USING DOCKER is working via:
+        <code>docker exec -it web-meeting-calendar-service php utils/dbSeederPostgresql.util.php</code>
+    </li>
+    <li>✅ PostgreSQL Migration USING DOCKER is working via:
+        <code>docker exec -it web-meeting-calendar-service php utils/dbMigratePostgresql.util.php</code>
+    </li>
+</ul>
 
-        <h2>MongoDB:</h2>
-        <?php include HANDLERS_PATH . 'mongodbChecker.handler.php'; ?>
-
-        <h2>PostgreSQL:</h2>
-        <?php include HANDLERS_PATH . 'postgreChecker.handler.php'; ?>
-
-        <h2>Automation:</h2>
-        <ul>
-            <li>✅ PostgreSQL Reset USING DOCKER is working via: <code>docker exec -it web-meeting-calendar-service php utils/dbResetPostgresql.util.php
-</code></li>
-            <li>✅ PostgreSQL Seeder USING DOCKER is working via: <code>docker exec -it web-meeting-calendar-service php utils/dbSeederPostgresql.util.php
-</code></li>
-            <li>✅ PostgreSQL Migration USING DOCKER is working via:
-                <code>docker exec -it web-meeting-calendar-service php utils/dbMigratePostgresql.util.php</code>
-            </li>
-        </ul>
-    </div>
-
-</body>
-
-</html>
+<?php
+$content = ob_get_clean();
+include BASE_PATH . '/layouts/main.layout.php';
