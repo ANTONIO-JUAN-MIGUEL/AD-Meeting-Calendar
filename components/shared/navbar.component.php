4897814314c866b0212b15cacb60bfa1cd3ce77e
MadmanMigz <202310011@fit.edu.ph>
2025-07-08 17:11:55 +0800

    refactor (navbar):updated code for new format and using required folders/files.

---
+++ components/shared/navbar.component.php
@@ -0,0 +1,8 @@
+<nav class="navbar">
+    <a href="/pages/status/index.php">Status</a>
+    <a href="/pages/login/index.php">Login</a>
+    <a href="/pages/signup/index.php">Signup</a>
+    <a href="/pages/account/index.php">Account</a>
+    <a href="/pages/users/index.php">Users</a>
+    <a href="/pages/logout/index.php">Logout</a>
+</nav>
