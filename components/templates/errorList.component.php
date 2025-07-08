9b7e5e6ef6459ed332babd35e7d8aa669ee12df7
MadmanMigz <202310011@fit.edu.ph>
2025-07-08 17:12:14 +0800

    feat (error): added errorList.

---
+++ components/templates/errorList.component.php
@@ -0,0 +1,7 @@
+<?php if (!empty($errors)): ?>
+    <ul class="error">
+        <?php foreach ($errors as $err): ?>
+            <li><?= htmlspecialchars($err) ?></li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
